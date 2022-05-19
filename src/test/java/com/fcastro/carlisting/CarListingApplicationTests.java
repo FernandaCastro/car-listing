a173617d0e837942732190fd33693419c91bc419
fernandacastro <fcastro.rj@gmail.com>
2022-05-19 11:08:59 +0200

    Initial Commit

---
+++ src/test/java/com/fcastro/carlisting/CarListingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcastro.carlisting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarListingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
